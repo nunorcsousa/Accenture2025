347646e97a53beb27d2a5aff1ae546211feaa1b9
Nuno Sousa <nunorcsousa@gmail.com>
2025-07-31 19:19:57 +0100

    Init JEE

---
+++ JSEProjectMaven/src/main/java/io/altar/jseproject/controllers/ProductController.java
@@ -0,0 +1,22 @@
+package io.altar.jseproject.controllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+@Path("products")
+public class ProductController {
+
+	@Context
+	protected UriInfo context;
+	
+	@GET
+	@Path("status")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String status() {
+		return "Url : " + context.getRequestUri().toString() + " is Ok";
+	}
+}
