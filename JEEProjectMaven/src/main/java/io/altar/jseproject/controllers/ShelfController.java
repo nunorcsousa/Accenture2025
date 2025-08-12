fb1c33fd93ae6fd88e066e5143feb8d14e4dcaf7
Nuno Sousa <nunorcsousa@gmail.com>
2025-08-12 17:49:57 +0100

    Correcção erro EndPoint Shelf

--- JEEProjectMaven/src/main/java/io/altar/jseproject/controllers/ShelfController.java
+++ JEEProjectMaven/src/main/java/io/altar/jseproject/controllers/ShelfController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import io.altar.jseproject.repositories.ShelfRepository;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -20,6 +18,7 @@ import javax.ws.rs.core.Response;
 import io.altar.jseproject.model.Shelf;
 import io.altar.jseproject.repositories.ShelfRepository;
 
+@Path("/shelves")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ShelfController {
